6b840f18bd9e6a5df073d0cd0297f40ed4d1f185
stonedjjh <danjim82@gmail.com>
2025-12-28 14:43:54 -0400

    Docs: documentación del tipo de datos Never

--- udemy/FernandoHerrera/BASES/tipos/never.ts
+++ udemy/FernandoHerrera/BASES/tipos/never.ts
@@ -1,1 +1,11 @@
-(() => {})();
+(() => {
+  /* función que nunca llega a un return
+   es decir, nunca termina su ejecución de forma correcta
+   o lo que es lo mismo no puede tener un punto final alcanzable
+   */
+  const abc = (message: string): never => {
+    throw new Error(message);
+  };
+
+  abc("auxilio");
+})();
