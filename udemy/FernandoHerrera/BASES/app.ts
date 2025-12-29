e5452e5a1dada9c6e2a0d3d93669e522d3d58a50
stonedjjh <danjim82@gmail.com>
2025-12-29 00:46:47 -0400

    Docs: Resolución del ejericio #2

--- udemy/FernandoHerrera/BASES/app.ts
+++ udemy/FernandoHerrera/BASES/app.ts
@@ -1,16 +1,17 @@
 // Funciones Básicas
-function sumar(a, b) {
+function sumar(a: number, b: number): number {
   return a + b;
 }
 
-const contar = (heroes) => {
+const contar = (heroes: string[]): number => {
   return heroes.length;
 };
-const superHeroes = ["Flash", "Arrow", "Superman", "Linterna Verde"];
+
+const superHeroes: string[] = ["Flash", "Arrow", "Superman", "Linterna Verde"];
 contar(superHeroes);
 
 //Parametros por defecto
-const llamarBatman = (llamar) => {
+const llamarBatman = (llamar: boolean = false): void => {
   if (llamar) {
     console.log("Batiseñal activada");
   }
@@ -19,13 +20,25 @@ const llamarBatman = (llamar) => {
 llamarBatman();
 
 // Rest?
-const unirheroes = (personas) => {
+const unirheroes = (...personas: string[]) => {
   return personas.join(", ");
 };
 
 // Tipo funcion
-const noHaceNada = (numero, texto, booleano, arreglo) => {};
+const noHaceNada = (
+  numero: number,
+  texto: string,
+  booleano: boolean,
+  arreglo: []
+) => {};
 
 // Crear el tipo de funcion que acepte la funcion "noHaceNada"
-let noHaceNadaTampoco;
+
+let noHaceNadaTampoco: (
+  numero: number,
+  texto: string,
+  booleano: boolean,
+  arreglo: []
+) => void;
+
 noHaceNadaTampoco = noHaceNada;
