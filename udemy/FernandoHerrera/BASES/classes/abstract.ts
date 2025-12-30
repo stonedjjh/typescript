c042c5aad9c17e01ab0735da947d745fb0d0bec0
Daniel Jimenez <danjim82@gmail.com>
2025-12-30 16:13:17 -0400

    Feature: trabajando con clases abstract

---
+++ udemy/FernandoHerrera/BASES/classes/abstract.ts
@@ -0,0 +1,31 @@
+(() => {
+
+    abstract class Mutante {
+        constructor(
+            public name: string,
+            public realName: string
+        ) {}
+    }
+
+    // No se puede instanciar una clase abstracta pero si podemos heredar de ella
+    class Xmen extends Mutante {
+        salvarMundo() {
+            return 'Mundo salvado!';
+        }
+    }
+    class Villian extends Mutante {
+        conquistarMundo() {
+            return 'Mundo conquistado!';
+        }
+    }
+
+    const printName = ( character: Mutante ) => {
+        console.log( character.realName );
+    }
+
+    const wolverine = new Xmen('Wolverine', 'Logan');
+    const magneto = new Villian('Magneto', 'Max Eisenhardt');
+
+    console.log( wolverine );
+    console.log( magneto );
+})()
