136fc10267e77eb37f52396e372767c1f1eedd73
stonedjjh <danjim82@gmail.com>
2025-12-29 21:17:41 -0400

    Docs: Agregación de documentación

--- udemy/FernandoHerrera/BASES/tipos/numbes.ts
+++ udemy/FernandoHerrera/BASES/tipos/numbes.ts
@@ -12,5 +12,6 @@
 
   /*en javascript se puede hacer esto, pero en typescript no se puede
     en javascript mostraria "Nos salvamos" 
+    en typescript da tipos/numbes.ts:7:7 - error TS18048: 'avenger' is possibly 'undefined'.
   */
 })();
