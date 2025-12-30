a2673e6fff9dbe71456a5f224555682e8505f7b5
Daniel Jimenez <danjim82@gmail.com>
2025-12-30 17:56:29 -0400

    Docs: cambios en app.ts ejericio #4

--- udemy/FernandoHerrera/BASES/app.js
+++ udemy/FernandoHerrera/BASES/app.js
@@ -1,8 +1,51 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const msg = "Hello, TypeScript!";
-console.log(msg);
-const hero = {
-    name: "Ironman",
-    age: 45,
-};
+// Crear interfaces
+
+// Cree una interfaz para validar el auto (el valor enviado por parametro)
+const conducirBatimovil = ( auto ):void => {
+  auto.encender = true;
+  auto.velocidadMaxima = 100;
+  auto.acelear();
+}
+
+const batimovil = {
+  encender:false,
+  velocidadMaxima:0,
+  acelear(){
+    console.log("...... gogogo!!!");
+  }
+}
+
+// Cree una interfaz con que permita utilzar el siguiente objeto
+// utilizando propiedades opcionales
+
+const guason = {
+  reir: true,
+  comer:true,
+  llorar:false
+}
+
+const reir = ( guason ):void => {
+  if( guason.reir ){
+    console.log("JAJAJAJA");
+  }
+}
+
+
+// Cree una interfaz para la siguiente funcion
+
+const ciudadGotica = ( ciudadanos:string[] ):number => {
+  return ciudadanos.length;
+}
+
+// Cree una interfaz que obligue crear una clase
+// con las siguientes propiedades y metodos
+
+/*
+  propiedades:
+    - nombre
+    - edad
+    - sexo
+    - estadoCivil
+    - imprimirBio(): void // en consola una breve descripcion.
+*/
+class Persona {}
