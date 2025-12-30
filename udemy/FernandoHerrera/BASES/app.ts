71cd1f93997978a9906f1d8ddb968efd79ea4e65
Daniel Jimenez <danjim82@gmail.com>
2025-12-30 18:15:29 -0400

    Feature: Implementación solución ejercicio #4

--- udemy/FernandoHerrera/BASES/app.ts
+++ udemy/FernandoHerrera/BASES/app.ts
@@ -1,13 +1,20 @@
 // Crear interfaces
 
+interface Auto {
+  encender: boolean;
+  velocidadMaxima: number;
+  acelear(): void;
+}
+
+
 // Cree una interfaz para validar el auto (el valor enviado por parametro)
-const conducirBatimovil = ( auto ):void => {
+const conducirBatimovil = ( auto : Auto ):void => {
   auto.encender = true;
   auto.velocidadMaxima = 100;
   auto.acelear();
 }
 
-const batimovil = {
+const batimovil: Auto = {
   encender:false,
   velocidadMaxima:0,
   acelear(){
@@ -18,13 +25,20 @@ const batimovil = {
 // Cree una interfaz con que permita utilzar el siguiente objeto
 // utilizando propiedades opcionales
 
-const guason = {
+interface Guason {
+  reir?: boolean;
+  comer?: boolean;
+  llorar?: boolean;
+}
+
+
+const guason : Guason = {
   reir: true,
   comer:true,
   llorar:false
 }
 
-const reir = ( guason ):void => {
+const reir = ( guason : Guason ):void => {
   if( guason.reir ){
     console.log("JAJAJAJA");
   }
@@ -33,7 +47,11 @@ const reir = ( guason ):void => {
 
 // Cree una interfaz para la siguiente funcion
 
-const ciudadGotica = ( ciudadanos:string[] ):number => {
+interface CiudadGotica {
+  ( ciudadanos: string[] ): number;
+}
+
+const ciudadGotica:CiudadGotica = ( ciudadanos:string[] ):number => {
   return ciudadanos.length;
 }
 
@@ -48,4 +66,29 @@ const ciudadGotica = ( ciudadanos:string[] ):number => {
     - estadoCivil
     - imprimirBio(): void // en consola una breve descripcion.
 */
-class Persona {}
+
+interface PersonaInterface {
+  nombre: string;
+  edad: number;
+  sexo: string;
+  estadoCivil: string;
+  imprimirBio(): void;
+}
+
+class Persona implements PersonaInterface {
+  nombre: string;
+  edad: number;
+  sexo: string;
+  estadoCivil: string;
+
+  constructor(nombre: string, edad: number, sexo: string, estadoCivil: string) {
+    this.nombre = nombre;
+    this.edad = edad;
+    this.sexo = sexo;
+    this.estadoCivil = estadoCivil;
+  }
+
+  imprimirBio(): void {
+    console.log(`Nombre: ${this.nombre}, Edad: ${this.edad}, Sexo: ${this.sexo}, Estado Civil: ${this.estadoCivil}`);
+  }
+}
