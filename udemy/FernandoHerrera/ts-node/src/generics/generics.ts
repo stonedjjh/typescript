55d33c4bfc9bf5bac10a6a771a899ca4ab7f9cc0
Daniel Jimenez <danjim82@gmail.com>
2026-05-28 12:31:53 -0400

    feat: implementacion de una funcion generica que permita aprovechar las caracteristicas del tipo de dato usado

---
+++ udemy/FernandoHerrera/ts-node/src/generics/generics.ts
@@ -0,0 +1,8 @@
+export const printObject = (argument:any) => {
+    console.log(argument);
+}
+
+
+export function genericFunction<T>(argument: T): T {
+    return argument;
+}
--- udemy/FernandoHerrera/ts-node/src/index.ts
+++ udemy/FernandoHerrera/ts-node/src/index.ts
@@ -1,8 +1,17 @@
-import { Hero } from './classes/Hero';
-
-const IronMan = new Hero('Iron Man', 1, 45);
-//IronMan.introduce();
+import { printObject, genericFunction } from './generics/generics';
 
 
+// Ejemplo del uso de la función printObject con diferentes tipos de argumentos
+printObject('Hello world');
+printObject(123);
+printObject(true);
 
-console.log(IronMan.power);
+const name:string = 'Daniel';
+
+// Ejemplo del uso de la función genérica genericFunction con diferentes tipos de argumentos
+
+console.log(genericFunction('Hello world').toUpperCase());
+console.log(genericFunction(123).toFixed());
+console.log(genericFunction(true).valueOf());
+console.log(genericFunction(new Date()).getDate ());
+
