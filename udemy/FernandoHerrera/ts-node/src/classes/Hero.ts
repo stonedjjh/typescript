21994110441f8a95fbb7bf3ee5a18092ca8f35ef
Daniel Jimenez <danjim82@gmail.com>
2026-05-28 11:40:23 -0400

    feat: Solución ejercicio sección 10

--- udemy/FernandoHerrera/ts-node/src/classes/Hero.ts
+++ udemy/FernandoHerrera/ts-node/src/classes/Hero.ts
@@ -1,11 +1,17 @@
+import { powers, type Power } from '../data/powers'
+
 export class Hero {
     constructor(
         public name: string,
-        public powerId: string,
+        public powerId: number,
         public age: number
     ) {}
 
     public introduce(): void {
         console.log(`Hi, I'm ${this.name}, and I have the power of ${this.powerId}. and i'm ${this.age} years old.`);
+    }
+
+    get power():string{
+        return powers.find((power:Power) => power.id === this.powerId)?.desc || 'not found';
     }
 }
--- udemy/FernandoHerrera/ts-node/src/index.ts
+++ udemy/FernandoHerrera/ts-node/src/index.ts
@@ -1,14 +1,8 @@
 import { Hero } from './classes/Hero';
-import { powers } from './data/powers';
 
-
-console.log('Hola Mundo!');
-
-
-
-const IronMan = new Hero('Iron Man', 'Tech Genius', 45);
-IronMan.introduce();
+const IronMan = new Hero('Iron Man', 1, 45);
+//IronMan.introduce();
 
 
 
-console.log(powers);
+console.log(IronMan.power);
