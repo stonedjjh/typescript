adae04645773537ccffa2152f987ebb7a29894a4
Daniel Jimenez <danjim82@gmail.com>
2026-05-28 16:42:55 -0400

    feat: Agrupacióon de interfaces

--- udemy/FernandoHerrera/ts-node/src/generics/generics.ts
+++ udemy/FernandoHerrera/ts-node/src/generics/generics.ts
@@ -6,3 +6,8 @@ export const printObject = (argument:any) => {
 export function genericFunction<T>(argument: T): T {
     return argument;
 }
+
+
+export const genericFunctionArrow = <T>(argument: T): T => {
+    return argument;
+}
--- udemy/FernandoHerrera/ts-node/src/index.ts
+++ udemy/FernandoHerrera/ts-node/src/index.ts
@@ -1,17 +1,28 @@
-import { printObject, genericFunction } from './generics/generics';
+import { printObject, genericFunction, genericFunctionArrow } from './generics/generics';
+
+import type { Hero, Villain } from './interfaces';
+
 
 
 // Ejemplo del uso de la función printObject con diferentes tipos de argumentos
-printObject('Hello world');
-printObject(123);
-printObject(true);
 
-const name:string = 'Daniel';
+// printObject('Hello world');
+// printObject(123);
+// printObject(true);
+
+// const name:string = 'Daniel';
 
 // Ejemplo del uso de la función genérica genericFunction con diferentes tipos de argumentos
 
-console.log(genericFunction('Hello world').toUpperCase());
-console.log(genericFunction(123).toFixed());
-console.log(genericFunction(true).valueOf());
-console.log(genericFunction(new Date()).getDate ());
+// console.log(genericFunction('Hello world').toUpperCase());
+// console.log(genericFunction(123).toFixed());
+// console.log(genericFunction(true).valueOf());
+// console.log(genericFunction(new Date()).getDate ());
 
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Wilson',
+    dangerLevel: 130
+}
+
+console.log( genericFunctionArrow<Hero>(deadpool).realName.toUpperCase() );
